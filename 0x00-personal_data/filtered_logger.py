e8982c66dc496e508b22fff7c32783216d748fe0
root <snowdbat@gmail.com>
2024-12-01 15:07:24 +0000

    Task0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
 import re
+from typing import List
 
-
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
     escaped_fields = (re.escape(field) for field in fields)
     fields_pattern = '|'.join(escaped_fields)
     pattern = rf"({fields_pattern})=[^{separator}]*"
